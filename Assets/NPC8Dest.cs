5e0041a4fc9b4ca16ec7e04cd3548e9d7415105d
Andreffff <a034875@ismai.pt>
2021-01-17 20:13:55 +0000

    criação de ExplorePanel e correção de pequenos pormenores

---
+++ Assets/NPC8Dest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC8Dest : MonoBehaviour
+{
+    public int pivotPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (pivotPoint == 2)
+        {
+            this.gameObject.transform.position = new Vector3(0, 17, -60);
+            pivotPoint = 0;
+        }
+        if (pivotPoint == 1)
+        {
+            this.gameObject.transform.position = new Vector3(0, 17, -41);
+            pivotPoint = 2;
+        }
+        if (pivotPoint == 0)
+        {
+            this.gameObject.transform.position = new Vector3(0, 17, -21);
+            pivotPoint = 1;
+        }
+    }
+
+}
